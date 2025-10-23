4fcf72173cfe45c362b6743d6ffb6d7ad0ca66e3
Sergey119 <manymail08@gmail.com>
2025-10-24 01:03:27 +0300

    commit person
    commit
    
    commit

---
+++ Person.java
@@ -0,0 +1,24 @@
+import java.util.Objects;
+
+public abstract class Person {
+    protected int id;
+    protected String name;
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
